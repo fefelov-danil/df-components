26354b61de6d3a103f51e878a3a8541d7d08fa2f
Daniil Fefelov <fefelov.danil@gmail.com>
2023-01-17 21:10:02 +0600

    feat: component filling for example

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,15 @@ export function App() {
 
   return (
     <div>
-      <DropDown openPopupChildren={<Button>11111</Button>}>
-        <p>Пункт один</p>
-        <p>Пункт два</p>
-        <p>Пункт три</p>
+      <Button>Пример кнопки </Button><br/><br/>
+      <Button colorType={'warning'}>Пример кнопки</Button><br/><br/>
+      <Button outlined={true}>Пример кнопки</Button><br/><br/>
+      <Button loading={true}>Пример кнопки</Button><br/><br/>
+      <Button text={true}>Пример кнопки</Button><br/><br/>
+      <DropDown openPopupChildren={<Button colorType={'success'}>Кнопка DropDown</Button>}>
+        <span>Пункт один<br/></span>
+        <span>Пункт два<br/></span>
+        <span>Пункт три</span>
       </DropDown>
     </div>
   );
